747935a6fcc5a08533ee516219cc7be90881379c
bsnlmahesh <mahesh.23joy@gmail.com>
2025-05-16 22:00:29 +0530

    provider added in main.tf

--- infra/test/main.tf
+++ infra/test/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.29.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+  # Configuration options
+}
+
+
+
 module "rg" {
     source = "../../rg"
     rg     = var.rg
